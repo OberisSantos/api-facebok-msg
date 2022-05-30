1d88560747238995ffdcbbb7fa9d27bb73b9d187
Oberis dos Santos Nascimento <oberis2010@gmail.com>
2022-05-30 09:24:03 -0300

    update

--- myapi/app.py
+++ myapi/app.py
@@ -17,6 +17,10 @@ def callSendAPI(senderPsid, response):
 def home():
     return 'Home'
 
+@app.route('/home', methods=["GET", "POST"])
+def home1():
+    return 'Home1'
+
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port='8888',debug=True)
     port  =  int( os.environ.get ( "PORT" ,  5000 )) 
